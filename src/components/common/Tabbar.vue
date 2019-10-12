0573c21010b396a23b92be13da88564079aec74b
xiolng <zheng_zhanning@126.com>
2019-10-12 17:36:15 +0800

    index

---
+++ src/components/common/Tabbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <van-tabbar v-model="active">
+            <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+            <van-tabbar-item icon="search">商城</van-tabbar-item>
+            <van-tabbar-item icon="friends-o">消息</van-tabbar-item>
+            <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                active: 2
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
