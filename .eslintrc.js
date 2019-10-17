c41c51b2d4f3ef7ef69192d9fda0996d4d7bdfa2
xiolng <zheng_zhanning@126.com>
2019-10-17 19:47:18 +0800

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'vue/no-parsing-error': [2, {'x-invalid-end-tag': false}]
+        'vue/no-parsing-error': [2, {'x-invalid-end-tag': false}],
+        'semi': ['error', 'never']
     },
     parserOptions: {
         parser: 'babel-eslint'
