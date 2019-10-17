56ecc11ab3a0ab9e60991b4e73a269e7b1ec13b8
xiolng <zheng_zhanning@126.com>
2019-10-17 19:36:25 +0800

    busimess

---
+++ src/views/activity/ActivityModule.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="activity_box pl_50 pr_50 tc">
+    <div class="white pt_10">充值金额：送话费+优惠券+服务</div>
+    <van-field placeholder="请输入充值号码" input-align="center" class="mobile_input" style="background: none;"></van-field>
+    <van-button type="warning" size="small" @click="">
+      立即参加
+    </van-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .activity_box{
+    min-height: 235px;
+    background: url("../../assets/activity_bg.png") left top no-repeat;
+    background-size: 100%;
+    padding-top: 62px;
+  }
+  .mobile_input{
+    background: none;
+    margin-bottom: 20px;
+    /deep/ input{
+      color: #ffffff;
+    }
+  }
+  .mobile_input ::placeholder{
+    color: #fff;
+  }
+</style>
---
+++ src/views/login/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
